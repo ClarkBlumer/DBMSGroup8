692b47144f22568f4986b45dc6f9db4dbb39d8b2
Jeffrey <Jeffrey@Friel-PC>
2016-04-09 15:11:17 -0500

    Added Bootstrap and jquery integration.  Created views/templates folder and added header.php and footer.php with code for bootstrap and jquery integration.

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+<!--Put footer info here-->
+
+        <script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+    </body>
+</html>
+
---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+${doctype}
+<!--
+CS 4830: Group 8
+CodeIgniter/BootStrap Template
+-->
+<html>
+    <head>
+        <meta charset="${project.encoding}">
+        <title>UM Course Sharing</title>
+
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+    </head>
+    <body>
+<!--Put header info here-->
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
