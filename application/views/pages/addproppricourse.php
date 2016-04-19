ad42ee274beceada77f23d2b6872d8dfe9fd7418
frielj <frielj@missouri.edu>
2016-04-19 09:10:35 -0500

    add logic to add multiple pri courses in form...needs to be formatted and query logic built

--- application/views/pages/addproppricourse.php
+++ application/views/pages/addproppricourse.php
@@ -24,78 +24,80 @@ foreach ($insertedProp as $value) {
 }
 ?>
     <h3>Enter Proposed Primary Course</h3>
-    <form class="form-horizontal" role="form" action="<?php echo base_url();?>index.php/addproposal/savePropPriCourse" method="post">
-        <?php echo "Prop id: ".$value['PROPID']."\n";?>
-        <input type="hidden" name="propid" value="<?php echo $value['PROPID']?>">
-        
-        <div class="form-group">
-            <label class="control-label col-xs-3 col-md-3" for="instituion">Institution</label>
-            <div class="col-xs-2 selectContainer">
-                <select  id="institution" name="institution" class="form-control">
-                    <!--Dynamically adding term values to dropdown-->
-                    <?php 
-                    foreach ($institution as $array) { ?>
-                            <option value="<?php echo $array['INSTITUTION'];?>"><?php echo $array['INSTIT_DESCR'];?></option>
-                    <?php
-                    }
-                    ?>
-                </select>
-             </div>
+ <div class="form-group">
+                    
+        <div class="multi-field-wrapper">
+            <div class="multi-fields">
+                <div class="multi-field">
+                    <div class="form-group">
+                        <h3>Proposed Primary Course</h3>
+                    </div>
+
+                    <div class="form-group">                                            
+                        <fieldset class="col-xs-2 selectContainer">
+                            <label class="control-label" for="prop_budget_requested">Institution</label>
+                            <select id="INSTITUTION" class="form-control" name="size">
+                                <!--Dynamically adding term values to dropdown-->
+
+                                 <?php 
+                                foreach ($institution as $array) { ?>
+                                        <option value="<?php echo $array['INSTITUTION'];?>"><?php echo $array['INSTIT_DESCR'];?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </fieldset>
+
+                        <fieldset class="col-xs-2 selectContainer">
+                            <label class="control-label" for="prop_budget_requested">Career</label>
+                            <select id="ACAD_CAREER" class="form-control" name="size">
+                                <!--Dynamically adding term values to dropdown-->
+                                 <?php 
+                                foreach ($career as $array) { ?>
+                                        <option value="<?php echo $array['ACAD_CAREER'];?>"><?php echo $array['DESCR'];?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </fieldset>
+
+                        <fieldset class="form-group col-md-3">                                                
+                            <label class="control-label" for="SUBJECT">Subject</label>
+                            <input id="SUBJECT" type="text" class="form-control input-sm">
+                            <label class="control-label" for="CATALOG_NUM">Catalog Number</label>   
+                            <input id="CATALOG_NUM" type="text" class="form-control">
+                            <label class="control-label" for="CATALOG_NUM">Course Title</label>   
+                            <input id="DESCR" type="text" class="form-control">
+                            <label class="control-label" for="COURSE_TOPIC">Course Topic</label>   
+                            <input id="COURSE_TOPIC" type="text" class="form-control">
+                        </fieldset>
+
+                    </div>
+                    <fieldset class="form-group col-md-3">
+                    <button type="button" class="remove-field btn btn-default">Remove</button>
+                    </fieldset>
+
+                </div>
 
 
+            </div>
+            <fieldset class="form-group col-md-3">
+                <button type="button" class="add-field btn btn-default">Add Primary Course</button>
+            </fieldset>
+            </div>
+            <fieldset class="form-group col-md-3">
+                <button type="button" class="add-field btn btn-default">Add Primary Course</button>
+            </fieldset>
+        </div>
 
-            <label class="control-label col-xs-3 col-md-3" for="career">Career</label>
-            <div class="col-xs-2 selectContainer">
-                <select  id="career" name="career" class="form-control">
-                    <!--Dynamically adding term values to dropdown-->
-                    <?php 
-                    foreach ($career as $array) { ?>
-                            <option value="<?php echo $array['ACAD_CAREER'];?>"><?php echo $array['DESCR'];?></option>
-                    <?php
-                    }
-                    ?>
-                </select>
-             </div>
-        </div>
-        
-        
-        <div class="form-group">        
-            <label class="control-label col-xs-3 col-md-3" for="subject">Subject</label>
-            <div class="col-xs-2">
-                <input id="subject" name="subject" type="text" class="form-control">
-            </div>
-        
-            <label class="control-label col-xs-3 col-md-3" for="catalog_num">Catalog Number</label>
-            <div class="col-xs-2">
-                <input id="catalog_num" name="catalog_num" type="text" class="form-control">
-            </div>
-        </div>
-        
-        <div class="form-group">        
-            <label class="control-label col-xs-3 col-md-3" for="descr">Course Title</label>
-            <div class="col-xs-9">
-                <input id="descr" name="descr" type="text" class="form-control">
-            </div>
-        </div>
-        
-        <div class="form-group">        
-            <label class="control-label col-xs-3 col-md-3" for="descr_topics">Course Topic</label>
-            <div class="col-xs-9">
-                <input id="descr_topics" name="descr_topics" type="text" class="form-control">
-            </div>
-        </div>
-        
-                
-                
-                
-                
-            
-  
-        
-        <button type="submit" class="btn btn-default">Submit</button>    
-    </form>
+        <!-- Form Buttons: Submit, Reset -->
+
+   
+    <fieldset class="form-group col-md-3">
+        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="reset" class="btn btn-danger">Reset</button> 
+    </fieldset>
 </div>
-
 <?php
 
 /* 
@@ -104,8 +106,33 @@ foreach ($insertedProp as $value) {
  * and open the template in the editor.
  */
 //var_dump($subject);
-var_dump($insertedProp);
+
 
 
 ?>
 
+<script>
+$(function()
+{
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+
+        var controlForm = $('.controls form:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove', function(e)
+    {
+		$(this).parents('.entry:first').remove();
+
+		e.preventDefault();
+		return false;
+	});
+});    
+</script>
