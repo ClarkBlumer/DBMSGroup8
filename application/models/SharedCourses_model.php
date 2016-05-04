b85037d7b2a1adf6551cb1a06f858ecc825cf6b5
rwfwcb <rwfwcb@mail.missouri.edu>
2016-05-04 15:12:53 -0500

    Added files via upload

--- application/models/SharedCourses_model.php
+++ application/models/SharedCourses_model.php
@@ -7,16 +7,12 @@
  */
 
 /**
- * Description of ProposalCourses_model
- *
- * @author Jeffrey
+ *   Description of SharedCourse_model
+ *   Author: Robert Fink
+ *   University of Missouri System Shared Courses Application
  */
-class SharedCourses_model extends CI_Model{
-    //const primarycoursetable = 'TBL_SHARED_PRI_COURSES';
-    //const secondarycoursetable = 'TBL_SHARED_SEC_COURSES';
-    //const propid = 'PROPID';
-    //const priseqnum = 'PRI_SEQ_NUM';
 
+class SharedCourses_model extends CI_Model{
 
     public function __construct() {
         parent::__construct();
@@ -27,7 +23,6 @@ class SharedCourses_model extends CI_Model{
         $this->db->select("*");
         $this->db->from("BBCRSSHARE.CS_PRI_CRSE");
         $this->db->where("INSTITUTION = '$institution'");
-        
         $query = $this->db->get();
         return $query->result_array();
     }
@@ -36,7 +31,6 @@ class SharedCourses_model extends CI_Model{
         $this->db->select("*");
         $this->db->from("BBCRSSHARE.CS_SEC_CRSE");
         $this->db->where("INSTITUTION = '$institution'");
-        
         $query = $this->db->get();
         return $query->result_array();
     }
@@ -49,10 +43,17 @@ class SharedCourses_model extends CI_Model{
         return $result1->result_array();
     }
     
+    function selectSecCourses(){
+        $this->db->select('*');
+        $this->db->from('TBL_SHARED_SEC_COURSES');
+        $this->db->where('PROPID', $this->session->PROPID);
+        $result2 = $this->db->get();
+        return $result2->result_array();
+    }
+    
      function insertPriCourse($data) {
         $this->db->trans_start();
         $this->db->set($data);
-        var_dump($data);
         $this->db->insert("TBL_SHARED_PRI_COURSES", $data);
         $this->db->trans_complete();
     }
@@ -60,23 +61,15 @@ class SharedCourses_model extends CI_Model{
     function insertSecCourse($data) {
         $this->db->trans_start();
         $this->db->set($data);
-        var_dump($data);
         $this->db->insert("TBL_SHARED_SEC_COURSES", $data);
         $this->db->trans_complete();
     }
     
-    
-    /*
-    function getClasses($institution, $term) {
-        $this->db->select("*");
-        $this->db->from("BBCRSSHARE.CS_PRI_CRSE");
-        $this->db->where('INSTITUTION', $institution);
-        $this->db->where('DESCRSHORT', $term);
-        
-        $query = $this->db->get();
-        return $query->result_array();
+    function insertXREF($data) {
+        $this->db->trans_start();
+        $this->db->set($data);
+        $this->db->insert("BBCRSSHARE.CRS_XREF", $data);
+        $this->db->trans_complete();
     }
-     */
-
-
+    
 }
