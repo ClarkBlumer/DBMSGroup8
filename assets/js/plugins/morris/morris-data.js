8cf72e30a1f9a2b6db283c9eaba64ad434ba9d83
Albehadili <Albehadili@users.noreply.github.com>
2016-05-07 12:36:32 -0700

    funding awarded function is finished

--- assets/js/plugins/morris/morris-data.js
+++ assets/js/plugins/morris/morris-data.js
@@ -1,42 +1,53 @@
 // Morris.js Charts sample data for SB Admin template
 
 $(function() {
+if (typeof js_array['UMCL'] == 'undefined'){UMCL=0;} else{UMCL=js_array['UMCL'];}
+if (typeof js_array['COLUM'] =='undfined'){COLUM=0;} else{(COLUM= js_array['COLUM']);}
+if (typeof js_array['UMKC']=='undefined'){UMKC =0;} else{UMKC=js_array['UMKC'];}
+if (typeof js_array['ROLLA']=='undefine'){ROLLA=0;} else{ROLLA = js_array['ROLLA'];}
 
 
-    // Donut Chart
+    // funding requested 
     Morris.Donut({
-        element: 'morris-donut-chart',
+        element: 'donut-primary-budget-campus',
         data: [{
             label: "Number of funding requested by MU",
-            value: 37
+            value: COLUM
+        }, {
+            label: "Number of funding requested by UMKC",
+            value: UMKC
         }, {
             label: "Number of funding requested by UMCL",
-            value: 33
-        }, {
-            label: "Number of funding requested by KMC",
-            value: 15
+            
+            value: UMCL
         }, {
             label: "Number of funding requested by ROLLA",
-            value: obj2
+            value: ROLLA
         }],
         resize: true
     });
 
 
+if (typeof fundAw['UMKC']=='undefined') {UMKC1=0;}else{UMKC=fundAw['UMKC'];}
+if(typeof fundAw['ROLLA'] =='undefined'){ROLLA=0;}else{ROLLA=fundAw['ROLLA'];}
+if(typeof fundAw['COLUM']=='undefined'){COLUM=0;}else{COLUM=fundAw['COLUM'];}
+if(typeof fundAw['UMCL']=='undefined'){UMCL=0;}else{UMCL=fundAw['UMCL'];}
+
+    // funding awarded 
     Morris.Donut({
         element: 'morris-line-chart',
         data: [{
             label: "Number of funding awarded to MU",
-            value: 37
+            value: COLUM
         }, {
             label: "Number of funding awarded to UMCL",
-            value: 33
+            value: UMCL
         }, {
-            label: "Number of funding awarded to KMC",
-            value: 15
+            label: "Number of funding awarded to UKMC",
+            value: UMKC
         }, {
             label: "Number of funding awarded to ROLLA",
-            value: 15
+            value: ROLLA
         }],
         resize: true
     });
