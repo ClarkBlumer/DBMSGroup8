e4440be63b0f46bf813eaf2a31ba8e56dff07ae8
Jeffrey <Jeffrey@Friel-PC>
2016-05-08 15:59:44 -0500

    created logic for reports page

---
+++ application/views/pages/newEmptyPHP.php
@@ -0,0 +1,15 @@
+            <?php 
+            foreach ($cols as $col) {?>
+            <?php echo $col;?>
+            <?php    
+            }
+            ?>
+       
+            <?php 
+            foreach ($reports as $val) {
+                foreach ($val as $key => $val) {?>
+                    <?php echo $val;?>
+                <?php}  
+            }
+            ?>
+            
